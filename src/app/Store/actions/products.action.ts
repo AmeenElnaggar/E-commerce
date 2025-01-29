5d31577d30798a61190a1eba9b01589ce4e28233
Ameen Sayed <ameenelnaggar9@gmail.com>
2025-01-30 01:03:20 +0200

    Delete Old Files Of Project

--- src/app/Store/actions/products.action.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Product } from '../../Shared/models/product.model';
-
-export const getProducts = createAction(
-  '[products] Get Avaliable Products',
-  props<{ products: Product[] }>()
-);
--- src/app/Store/selectors/products.selector.ts
+++
@@ -1,5 +0,0 @@
-import { StoreInterface } from '../store';
-
-export const productsSelector = (state: StoreInterface) => {
-  return state.products.allProducts;
-};
